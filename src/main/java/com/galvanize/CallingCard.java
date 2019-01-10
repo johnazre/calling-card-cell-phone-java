6ecc628d5bdccfa1bcda3d8988f7c58e9ac6cdbb
John <jaboyak@gmail.com>
2019-01-09 22:48:19 -0700

    cleaned out guts

--- src/main/java/com/galvanize/CallingCard.java
+++ src/main/java/com/galvanize/CallingCard.java
@@ -1,22 +1,5 @@
 package com.galvanize;
 
 public class CallingCard {
-    private int centsPerMinute;
-    private float minutesLeft;
-    public CallingCard(int centsPerMinute) {
-        this.centsPerMinute = centsPerMinute;
-    }
 
-    public void addDollars(int dollars) {
-        this.minutesLeft += (dollars * 100 / centsPerMinute);
-    }
-
-    public float getRemainingMinutes() {
-        return this.minutesLeft;
-    }
-
-    public void useMinutes(int minutes) {
-        float result = this.minutesLeft - minutes;
-        this.minutesLeft = result < 0 ? 0 : result;
-    }
 }
--- src/test/java/com/galvanize/CallingCardTest.java
+++ src/test/java/com/galvanize/CallingCardTest.java
@@ -8,38 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CallingCardTest {
     @Test
     public void canAddMoneyToCard() {
-        CallingCard card = new CallingCard(20);
-        card.addDollars(1);
-        assertEquals(5, card.getRemainingMinutes());
+
     }
 
     @Test
     public void canCheckRemainingMinutes() {
-        CallingCard card = new CallingCard(20);
-        card.addDollars(1);
-        assertEquals(5, card.getRemainingMinutes());
+
     }
 
     @Test
     public void canDecreaseMinutes() {
-        CallingCard card = new CallingCard(20);
-        card.addDollars(1);
-        card.useMinutes(3);
-        assertEquals(2, card.getRemainingMinutes());
+
     }
 
     @Test
     public void cannotDecreaseMinutesBelowZero() {
-        CallingCard card = new CallingCard(20);
-        card.addDollars(1);
-        card.useMinutes(6);
-        assertEquals(0, card.getRemainingMinutes());
+
     }
 
     @Test
     public void removeDecimalPlacesFromUnevenNumbers() {
-        CallingCard card = new CallingCard(13);
-        card.addDollars(1);
-        assertEquals(7, card.getRemainingMinutes());
+
     }
 }
